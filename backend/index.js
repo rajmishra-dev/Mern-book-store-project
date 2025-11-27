ffa881c254a99e71eab7da910a60c78cc51c8a20
rajmishra-dev <shradkumar002@gmail.com>
2025-11-27 17:26:02 +0000

    ✅ Implemented POST /books to save new books and GET /books to fetch all books from MongoDB

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,17 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// Route for Get all Books from database
+app.get("/books", async (request , response) => {
+    try{
+      const books = await Book.find({});
+      return response.status(200).json(books);
+    }catch(error){
+      console.log(error.message);
+      response.status(500).send({message: error.message});
+    };
+    
+});
 // Database connection
 mongoose
   .connect(mongoDBURL)
